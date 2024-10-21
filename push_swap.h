54f940d460798eab86b7c93a20843c349719010d
Elena Lavricheva <elavrich@c2r3s1.42prague.com>
2024-10-21 16:49:42 +0200

    pizza pasta vaf

--- push_swap.h
+++ push_swap.h
@@ -1,18 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elavrich <elavrich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/21 16:09:47 by elavrich          #+#    #+#             */
-/*   Updated: 2024/10/21 16:42:57 by elavrich         ###   ########.fr       */
+/*   Created: 2024/10/21 16:42:36 by elavrich          #+#    #+#             */
+/*   Updated: 2024/10/21 16:49:21 by elavrich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-int	main(void)
-{
 
-}
+#endif
