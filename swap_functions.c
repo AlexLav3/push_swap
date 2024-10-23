b060a84de306114b370e49f7121b5a56c967b4f8
Elena Lavricheva <elavrich@c2r2s1.42prague.com>
2024-10-23 17:01:11 +0200

    I am unsure what approach to take

--- swap_functions.c
+++ swap_functions.c
@@ -6,7 +6,7 @@
 /*   By: elavrich <elavrich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 16:38:10 by elavrich          #+#    #+#             */
-/*   Updated: 2024/10/23 16:44:09 by elavrich         ###   ########.fr       */
+/*   Updated: 2024/10/23 16:58:36 by elavrich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,30 @@
 
 void	sa(t_stack **stacka)
 {
-	t_stack	*tmpnbr;
-	t_stack	*tmpnbr2;
+	t_stack	*first;
+	t_stack	*second;
 
-	tmpnbr2 = stacka->nbr;
-	tmpnbr = stacka->next;
+	first = *stacka;
+	second = first->next;
+	first->next = second->next;
+	second->next = first;
+	second->prev = NULL;
+	*stacka = second;
 }
 void	sb(t_stack **stackb)
 {
-	t_stack	*tmpnbr;
-	t_stack	*tmpnbr2;
+	t_stack	*first;
+	t_stack	*second;
 
-	tmpnbr2 = stackb->nbr;
-	tmpnbr = stackb->next;
+	first = *stackb;
+	second = first->next;
+	first->next = second->next;
+	second->next = first;
+	second->prev = NULL;
+	*stackb = second;
 }
 void	ss(t_stack **stacka, t_stack **stackb)
 {
-	sa(**stacka);
-	sb(**stackb);
+	sa(stacka);
+	sb(stackb);
 }
